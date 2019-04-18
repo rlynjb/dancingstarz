eb2cbce51ff0e2f5cda82897d4a283e5f4136e32
rlyn <rlynjb@gmail.com>
2019-04-18 08:28:46 -0700

    update admin photo uploader

--- src/pages/admin/Index.vue
+++ src/pages/admin/Index.vue
@@ -1,16 +1,27 @@
 <template>
 <q-page class="q-pa-lg">
   admin area
-  <q-list class="row">
-  	<q-item class="col-2" v-for="(photo, index) in photos" :key="index">
-  		<h6 class="block">{{ photo.filename }}</h6>
-  		<br>
-  		<img :src="photo.url" />
+  <q-card class="q-mb-lg">
+  	<q-card-main>
+			<q-uploader :url="url" />
+		</q-card-main>
+	</q-card>
+
+  <q-list class="">
+  	<q-item v-for="(photo, index) in photos" :key="index">
+  		<q-item-side>
+  			<img :src="photo.url" />
+  		</q-item-side>
+
+  		<q-item-main>
+  			<h6 class="block">{{ photo.filename }}</h6>	
+  		</q-item-main>
+
+  		<q-item-side>
+				<q-btn @click="removePhoto">Remove</q-btn>
+  		</q-item-side>
   	</q-item>
   </q-list>
-
-
-  <q-uploader :url="url" />
 </q-page>
 </template>
 
@@ -28,6 +39,12 @@ export default {
   	photos() {
   		return this.$store.state.photos
   	}
+  },
+
+  methods: {
+  	removePhoto() {
+  		//
+  	},
   }
 }
 </script>
@@ -36,6 +53,6 @@ export default {
 @import '~variables'
 .q-item
 	img
-		width: 100%
+		max-width: 10em
 
 </style>
